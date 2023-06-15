6db48080c1394d250115782cafe11e0f6cc4610d
Aayushi Chaudhry <Aayushi.Chaudhry@bd.com>
2023-06-15 13:10:13 -0700

    trial

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+(function(window) {
+    window.extractData = function() {
+        var ret = $.Deferred();
 
-app.get('/index.html', (req, res) => {
-    const authorizationCode = req.query.code;
-})
+        function onError() {
+            console.log('Loading error', arguments);
+            ret.reject();
+        }
+
+        function onReady(app) {
+            if (app.hasOwnProperty('patient')) {
+                var patient = app.patient;
+                var pt = patient.read();
+
+                $.when(pt).fail(onError);
+                $.when(pt).done(function(patient){
+                    var genderx = patient.gender;
+                    var p = {gender: {value: genderx}}
+                    ret.resolve(p);
+                })
+            } else {
+                onError();
+            }
+        }
+    }
+
+    FHIR.oauth2.ready(onReady, onError); 
+    return ret.promise();
+}) (window);
