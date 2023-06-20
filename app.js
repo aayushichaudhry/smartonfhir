84b2c86069d42148bf4ce2596ae9d546f644faba
Aayushi Chaudhry <Aayushi.Chaudhry@bd.com>
2023-06-20 13:45:18 -0700

    got all of the information

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
 (function(window) {
     window.extractData = function() {
-        //var ret = $.Deferred();
-
-        // function onError() {
-        //     console.log('Loading error', arguments);
-        //     //ret.reject();
-        // }
         function processData(patient) {
             p = {}
             p['gender'] = patient['gender'];
-            p['birth date'] = patient['birthDate']
+            p['birth date'] = patient['birthDate'];
+            p['first name'] = patient['name'][0]['family'];
+            p['last name'] = patient['name'][0]['given'];
             return p;
         }
         function onReady(client) {
-            var p = {}
-            var patient = client.patient.read().then(processData);
-
-            // var genderx = patient[gender];
-            // var birthdatex = patient[birthDate];
-            // console.log(patient[name]);
-            //var firstNamex = patient.name[0].given;
-            //var lastNamex = patient.name[0].family;
-            //console.log(birthdatex);
-            //console.log(firstNamex);
-            //console.log(lastNamex);
-            //var p = {
-                //firstName: firstNamex, //{value: firstNamex},
-                //lastName: lastNamex, //{value: lastNamex},
-                //birthdate: birthdatex, //{value: birthdatex},
-                //gender: genderx, //{value: genderx}
-            //};
             return client.patient.read().then(processData);
         }
         return FHIR.oauth2.ready().then(onReady).catch(function(e) {
